26cf5166da77d0b26280d7af113cb363652cf8ea
Gaurav Jaiswal <gauravjaiswal5200@gmail.com>
2025-05-25 09:00:05 +0530

    deploy issue 2

--- client/src/pages/BlogList.jsx
+++ client/src/pages/BlogList.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import api from '../context/AuthContext';
+import { api } from '../context/AuthContext';
 
 function BlogList() {
   const [blogs, setBlogs] = useState([]);
